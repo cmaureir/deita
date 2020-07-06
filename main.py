9c4a3887717fe2d15e58188da79ec6c30393e719
Cristian Maureira-Fredes <Cristian.Maureira-Fredes@qt.io>
2020-07-06 19:13:38 +0200

    Add collapsable menu

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PySide2.QtCore import QSize, Slot
+from PySide2.QtCore import QSize, Slot, QPropertyAnimation, QObject, QByteArray
 from PySide2.QtWidgets import (QApplication, QMainWindow, QHBoxLayout,
                                QWidget, QPushButton, QTabWidget, QListWidget,
                                QListWidgetItem, QSizePolicy, QAction, QStyle)
@@ -15,11 +15,12 @@ class DWidget(QWidget):
         self.menu_level = 0
         self.item_list = QListWidget()
         self.fill_main_item_list()
-        self.item_list.setMinimumWidth(256)
+        #self.item_list.setMinimumWidth(256)
         self.item_list.setMaximumWidth(300)
-        #self.item_list.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.item_list.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.item_list.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
         self.item_list.itemClicked[QListWidgetItem].connect(self.item_clicked)
+
 
         ##################################################### Right
         b = QTabWidget()
@@ -28,7 +29,6 @@ class DWidget(QWidget):
         b.setMinimumWidth(768)
         #b.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         b.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
-
 
         hlayout.addWidget(self.item_list)
         hlayout.addWidget(b)
@@ -55,7 +55,6 @@ class DWidget(QWidget):
             self.menu_level = 0
             self.fill_main_item_list()
 
-
     def fill_main_item_list(self):
         for i in range(10):
             item = QListWidgetItem("Menu {}".format(i))
@@ -66,6 +65,7 @@ class DWidget(QWidget):
 class DWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
+        self.shown = True
 
          # Menu
         self.menu = self.menuBar()
@@ -80,18 +80,43 @@ class DWindow(QMainWindow):
 
         self.file_menu.addAction(exit_action)
 
+        self.show_action = QAction("Hide", self)
+        self.show_action.triggered.connect(self.toggle_left)
+        self.file_menu.addAction(self.show_action)
+
         # Status Bar
         self.status = self.statusBar()
         self.status.showMessage("a nice message to show")
 
-        w = DWidget()
+        self.w = DWidget()
         #geometry = app.desktop().availableGeometry(self)
         #self.setFixedSize(geometry.width() * 0.8, geometry.height() * 0.7)
-        self.setCentralWidget(w)
+        self.animation = QPropertyAnimation(self.w.item_list, QByteArray(b"maximumWidth"))
+        self.setCentralWidget(self.w)
 
     @Slot()
     def exit_app(self, checked):
         qApp.quit()
+
+    @Slot()
+    def toggle_left(self):
+        if self.shown:
+            self.animation.setDuration(100)
+            self.animation.setStartValue(self.w.item_list.width())
+            self.animation.setEndValue(0)
+            self.animation.start()
+            #self.w.item_list.hide()
+            self.show_action.setText("Show")
+            self.shown = False
+        else:
+            self.animation.setDuration(100)
+            self.animation.setStartValue(0)
+            self.animation.setEndValue(300)
+            self.animation.start()
+            #self.w.item_list.show()
+            self.show_action.setText("Hide")
+            self.shown = True
+
 
 
 def main():
